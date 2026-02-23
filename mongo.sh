b9a401c789da700da35f14751a446da1f7d2f30b
unknown <dasariravid145@gmail.com>
2026-02-23 17:36:04 +0530

    roboshop instace creation script

--- mongo.sh
+++ mongo.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
 USERID=$(id -u)
-FILE_FOLDER="/var/log/shell-script"
+FILE_FOLDER="/var/log/shell-roboshop"
 FILE_LOG="$FILE_FOLDER/$0.sh"
-MONGO="mongo.repo"
 
 if [ $USERID -nq 0 ]; then
     echo "Package Installation with Super Root User $USERID"
@@ -18,6 +17,7 @@ validate(){
         echo "$2....SUCCESS"
     fi
 }
+
 cp  mongo.repo /etc/yum.repos.d/mongo.repo | tee -a $FILE_LOG
 validate $? "copying.."
 
